571d8b5d75a37395a7f53627e350c8f04c8f7834
Nick Wallingford <nick@wallingford.cc>
2015-12-06 17:01:52 -0800

    Staging project_003. Need to make the array utilities work first.

--- src/project_003.asm
+++ src/project_003.asm
@@ -1,4 +1,14 @@
 global project_003
+extern print_array_ui
+
+section .data
+	array dq 8,14,27,23654,273
+
 section .text
 project_003:
+	mov rsi,array
+	mov rdx,5
+	call print_array_ui
+
+	mov rax,7821378213
 	ret
