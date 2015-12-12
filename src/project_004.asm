8c9ace9f4e48d203f5a1a2d90c0b5addd462babc
Nick Wallingford <nick@wallingford.cc>
2015-12-12 14:03:57 -0800

    Improved project 4. Now it's much more general.

--- src/project_004.asm
+++ src/project_004.asm
@@ -1,60 +1,51 @@
 global project_004
 
-section .bss
-num_string: resq 1
-
 section .text
 check_palindrome:
-	mov rdi,num_string
-	mov ebx,10
+	; Receives:
+	;  rax - the number the check.
+	; Returns:
+	;  rax - zero if palindrome, non-zero if non-palindrome.
+	; Requires:
+	;  rbx rdx r12 r13
+	mov rbx,10
+	mov r12,rax
+	xor r13,r13
+.L1:	xor rdx,rdx
+	div rbx
 
-	mov ecx,6
-.L1:	xor edx,edx
-	div ebx
-	mov [rdi],dl
-	inc rdi
-	loop .L1
+	lea r13,[r13+4*r13]  ; r13 *= 5   ; Cheaty way to mul by 10
+	shl r13,1            ; r13 *= 2
+	add r13,rdx
 
-	mov al,[num_string+1]
-	mov bl,[num_string+4]
-	mov ah,[num_string+2]
-	mov bh,[num_string+3]
-	cmp ax,bx
-	jne .false
+	test rax,rax
+	jnz .L1
 
-	mov al,[num_string+5]
-	mov bl,[num_string]
-	cmp al,bl
-	jne .false
-
-	mov al,1
-	ret
-.false:	xor rax,rax
+	mov rax,r12
+	xor rax,r13
 	ret
 
 project_004:
-	xor r8,r8 ; Largest known palindrome
-	mov r9d,1000 ; First multiplier + 1
+	xor r8,r8   ; Largest currently known palindrome. Zero is trivially a palindrome.
+	mov r9,1000 ; First multiplier + 1
 
-.L1:	dec r9d
-	mov eax,r9d ; Test to see if the current top triple is too small to possibly return a new maximum palindrome.
-	mul eax
-	cmp eax,r8d
+.L1:	dec r9
+	mov rax,r9 ; Test to see if the current top triple is too small to possibly return a new maximum palindrome.
+	mul rax
+	cmp rax,r8
 	jna .quit
 
-	mov r10d,r9d ; Second multiplier
-.L2:	dec r10d
-	mov eax,r10d
-	mul r9d
-	cmp eax,r8d
+	mov r10,r9 ; Second multiplier
+.L2:	dec r10
+	mov rax,r10
+	mul r9
+	cmp rax,r8
 	jna .L1
 
-	mov r11d,eax
+	mov r11,rax
 	call check_palindrome
-	test al,al
-	jz .L2
-
-	mov r8d,r11d
+	test rax,rax
+	cmovz r8,r11
 	jmp .L2
 
 .quit:	mov rax,r8
